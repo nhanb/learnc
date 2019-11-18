5f6b9b25460f0e71556ef89a726eaed7d4120814
Bùi Thành Nhân <hi@imnhan.com>
2019-11-18 22:05:41 +0700

    hello world

---
+++ ex03.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  char age = 's';
+  int height = 72;
+
+  printf("I am %d years old.\n", age);
+  printf("I am %d centimeters high.\n", height);
+
+  return 0;
+}
